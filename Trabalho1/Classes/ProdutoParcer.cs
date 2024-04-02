65138e5d2e089c7a737e7bf6c6c1fe9578091049
Usuário <Usurio@NOTJUCA>
2024-04-02 14:49:29 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho1/Classes/ProdutoParcer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Trabalho1.Classes
+{
+    public class ProdutoParcer
+    {
+
+        public enum Header
+        {
+            Codigo = 0,
+            descricao = 1,
+            categoria = 2,
+            preco = 3,
+            estoque = 4,
+            QtdVendida = 5,
+
+        }
+        public static List<Produto> ConverterLista(string arquivo)
+        {
+            List<Produto> produtos = new();
+
+            var linhas = arquivo.Split('\n').ToList();
+
+            linhas.Remove(linhas.First());
+            
+            foreach (var linha in linhas)
+            {
+                Produto produto = new()
+                {
+                    Codigo = Convert.ToInt32(linha.Split(';')[0]),
+                    Descricao = linha.Split(';')[1],
+                    Categoria = linha.Split(';')[2],
+                    Preco = Convert.ToDouble(linha.Split(';')[3]),
+                    Estoque = Convert.ToInt32(linha.Split(';')[4]),
+                    QtdVendida = Convert.ToInt32(linha.Split(';')[5])
+                    };
+
+                produtos.Add(produto);
+            }
+            return produtos;
+        }
+    }
+
+}
---
+++ Trabalho1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using Trabalho1.Classes;
+
+var dataSet = File.ReadAllText("..\\..\\..\\dataset.csv");
+
+var listaProdutos = ProdutoParcer.ConverterLista(dataSet);
+
+static void MaisVendidos(List<Produto> produtos)
+{
+    var produtosOrdenados = produtos.OrderByDescending(p => p.QtdVendida);
+
+    var top5Maisvendidos = produtosOrdenados.Take(5);
+
+    Console.WriteLine("Os 5 produtos mais vendidos são:");
+    foreach (Produto produto in top5Maisvendidos)
+    {
+        Console.WriteLine(String.Format("Código {0} - {1} com {2} unidades vendidas", produto.Codigo, produto.Descricao, produto.QtdVendida));
+    }
+}
+
+static void ImprimirProdutosComMaisEstoque(List<Produto> produtos, int quantidade)
+{
+    var produtosOrdenados = produtos.OrderByDescending(p => p.Estoque);
+
+    var topProdutos = produtosOrdenados.Take(3);
+
+    Console.WriteLine($"Os {quantidade} produtos com mais estoque são:");
+    foreach (var produto in topProdutos)
+    {
+        Console.WriteLine($"Nome: {produto.Descricao}, Estoque: {produto.Estoque}");
+    }
+}
+
+bool sair = false;
+while (!sair)
+{
+    Console.WriteLine(" ");
+    Console.WriteLine("Menu de Opções:");
+    Console.WriteLine("1. Produtos mais Vendidos");
+    Console.WriteLine("2. Produtos com mais Estoque");
+    Console.WriteLine("3. Categoria mais Vendida");
+    Console.WriteLine("4. Produtos menos Vendidos");
+    Console.WriteLine("5. Estoque de Segurança");
+    Console.WriteLine("6. Excesso de Estoque");
+    Console.WriteLine("7. Media de Preço por Caegoria");
+    Console.WriteLine("8. Sair");
+    Console.Write("Escolha uma opção: ");
+
+    string escolha = Console.ReadLine();
+
+    switch (escolha)
+    {
+        case "1":
+            MaisVendidos(listaProdutos);
+            break;
+        case "2":
+            ImprimirProdutosComMaisEstoque(listaProdutos, 3);            
+            break;
+        case "3":
+        
+            break;
+        case "4":
+        
+            break;
+        case "5":
+
+            break;
+        case "6":
+
+            break;
+        case "7":
+
+            break;
+        case "8":
+            Console.WriteLine("Saindo...");
+            sair = true;
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+            break;
+    }
+}
